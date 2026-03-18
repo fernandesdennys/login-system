cdbcb51fc6fc1235580990a766c1a2d7a00c294a
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-03-18 19:53:58 -0300

    UserDTO and RoleDTO implements

---
+++ back-end/src/main/java/io/github/fernandesdennys/back_end/dto/UserDTO.java
@@ -0,0 +1,65 @@
+package io.github.fernandesdennys.back_end.dto;
+
+import io.github.fernandesdennys.back_end.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Campo Obrigatório")
+    private String name;
+
+    @Email(message = "Favor entar com um email válido")
+    private String email;
+
+    Set<RoleDTO> roles =  new HashSet<>();
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+        entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<RoleDTO> getRoles() {
+        return roles;
+    }
+}
