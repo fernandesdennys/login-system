4f7a31d6d5749fc57ad47eabdf711213023a8480
Dennys Fernandes <dennys.projetos@outlook.com>
2026-03-23 16:55:06 -0300

    chore: minor adjustments and code improvements

--- front-end/src/pages/auth/login/Login.jsx
+++ front-end/src/pages/auth/login/Login.jsx
@@ -1,26 +1,41 @@
 import { FaUser, FaLock } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import './Login.css';
 
+function handleLogin(e) {
+  e.preventDefault();
+  fetch("Email:", email);
+  fetch("Password:", password);
+}
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleLogin}>
         <h1>LOGIN</h1>
 
         {/* USERNAME */}
-        <div className="input-field relative">
+        <div className="input-field relative text-white">
           <input
-            type="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder=" "
             required
             className="peer w-full border-b-2 border-gray-300 bg-transparent outline-none py-2"/>
 
-          <label className="absolute left-5 top-0 text-gray-500 transition-all
+          <label className="absolute left-5 -top-5 text-gray-500 transition-all
             peer-placeholder-shown:top-3
             peer-placeholder-shown:text-base
             peer-focus:-top-5
             peer-focus:text-sm
-            peer-focus:text-blue-500">
+            peer-focus:text-blue-500
+            peer-valid:-top-5
+            peer-valid:text-sm">
             Username
           </label>
 
@@ -28,19 +43,23 @@ function Login() {
         </div>
 
         {/* PASSWORD */}
-        <div className="input-field relative">
+        <div className="input-field relative text-white">
           <input
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder=" "
             required
-            className="peer w-full border-b-2 border-gray-300 bg-transparent outline-none py-2"/>
+            className="peer w-full border-b-2 border-gray-300 bg-transparent outline-none"/>
 
-          <label className="absolute left-5 top-0 text-gray-500 transition-all
+          <label className="absolute left-5 -top-5 text-gray-500 transition-all
             peer-placeholder-shown:top-3
             peer-placeholder-shown:text-base
             peer-focus:-top-5
             peer-focus:text-sm
-            peer-focus:text-blue-500">
+            peer-focus:text-blue-500
+            peer-valid:-top-5
+            peer-valid:text-sm">
             Password
           </label>
 
@@ -48,14 +67,12 @@ function Login() {
         </div>
 
         <div className="box-button">
-          <button className="btn-login">LOGIN</button>
+          <button type='submit' className="btn-login">LOGIN</button>
         </div>
 
         <div className="text-register">
-          <a href="/register">Create account</a>
-          <a href="#" className="link">
-            Forgot Password?
-          </a>
+          <Link to="/register">Create account</Link>
+          <Link to="#" className="link">Forgot Password?</Link>
         </div>
       </form>
     </div>
